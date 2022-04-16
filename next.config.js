e3962208bb7456973f36b97629c0ab3d9ce8a24e
Armyja <546323405@qq.com>
2022-04-16 18:28:43 +0800

    feat: init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
+  assetPrefix: isProd ? '' : ''
 }
 
 module.exports = nextConfig
